616387ebd209d6ab119d5b4b808be39a1b26e89a
Sophie Shan <jingins09@gmail.com>
2017-03-13 00:59:37 +0800

    Welcome all finished except back end

--- scripts.js
+++ scripts.js
@@ -140,3 +140,146 @@ $( document ).ready(function() {
       sendMessage();
     })
 });
+
+/*------sign up tab--------*/
+var marital; var kid; var stage;
+function single(){
+	document.getElementById("marital-single").style.backgroundColor="#ff89a0";
+	document.getElementById("marital-single").style.color="white";
+	document.getElementById("marital-married").style.backgroundColor="white";
+	document.getElementById("marital-married").style.color="#ff89a0";
+	
+	marital = 0;
+}
+
+function married(){
+	document.getElementById("marital-married").style.backgroundColor="#ff89a0";
+	document.getElementById("marital-married").style.color="white";
+	document.getElementById("marital-single").style.backgroundColor="white";
+	document.getElementById("marital-single").style.color="#ff89a0";
+	marital = 1;
+}
+
+function haveKid(){
+	document.getElementById("kid-yes").style.backgroundColor="#ff89a0";
+	document.getElementById("kid-yes").style.color="white";
+	document.getElementById("kid-no").style.backgroundColor="white";
+	document.getElementById("kid-no").style.color="#ff89a0";
+	kid = 1;
+}
+
+function noKid(){
+	document.getElementById("kid-no").style.backgroundColor="#ff89a0";
+	document.getElementById("kid-no").style.color="white";
+	document.getElementById("kid-yes").style.backgroundColor="white";
+	document.getElementById("kid-yes").style.color="#ff89a0";
+	kid = 0;
+}
+
+var deselectBg = "white"; var selectBg = "#ff89a0";
+var deselectTx = "#ff89a0"; var selectTx = "white";
+function clearStageSelection(){
+	document.getElementById("stage-one").style.backgroundColor=deselectBg;
+	document.getElementById("stage-two").style.backgroundColor=deselectBg;
+	document.getElementById("stage-three").style.backgroundColor=deselectBg;
+	document.getElementById("stage-four").style.backgroundColor=deselectBg;
+	document.getElementById("stage-one").style.color=deselectTx;
+	document.getElementById("stage-two").style.color=deselectTx;
+	document.getElementById("stage-three").style.color=deselectTx;
+	document.getElementById("stage-four").style.color=deselectTx;
+}
+
+function stageOne(){
+	clearStageSelection();
+	document.getElementById("stage-one").style.backgroundColor=selectBg;
+	document.getElementById("stage-one").style.color=selectTx;
+	stage = 1;
+}
+
+function stageTwo(){
+	clearStageSelection();
+	document.getElementById("stage-two").style.backgroundColor=selectBg;
+	document.getElementById("stage-two").style.color=selectTx;
+	stage = 2;
+}
+
+function stageThree(){
+	clearStageSelection();
+	document.getElementById("stage-three").style.backgroundColor=selectBg;
+	document.getElementById("stage-three").style.color=selectTx;
+	stage = 3;
+}
+
+function stageFour(){
+	clearStageSelection();
+	document.getElementById("stage-four").style.backgroundColor=selectBg;
+	document.getElementById("stage-four").style.color=selectTx;
+	stage = 4;
+}
+
+/*---Tags---*/
+var chemo=0; var medicine=0; var hospital=0; var surgery=0; var fam=0; var ref=0;
+var selectBorder="thin dashed #ff89a0";
+var deselectBorder="none";
+function chemoTag(){
+	if (chemo == 0){
+		document.getElementById("firstchpt-tag-chemo").style.border=selectBorder;
+		chemo = 1;
+	}
+	else{
+		document.getElementById("firstchpt-tag-chemo").style.border=deselectBorder;
+		chemo = 0;
+	}
+}
+
+function medicineTag(){
+	if (medicine == 0){
+		document.getElementById("firstchpt-tag-medicine").style.border=selectBorder;
+		medicine = 1;
+	}
+	else{
+		document.getElementById("firstchpt-tag-medicine").style.border=deselectBorder;
+		medicine = 0;
+	}
+}
+
+function hospitalTag(){
+	if (hospital == 0){
+		document.getElementById("firstchpt-tag-hospital").style.border=selectBorder;
+		hospital = 1;
+	}
+	else{
+		document.getElementById("firstchpt-tag-hospital").style.border=deselectBorder;
+		hospital = 0;
+	}
+}
+function surgeryTag(){
+	if (surgery == 0){
+		document.getElementById("firstchpt-tag-surgery").style.border=selectBorder;
+		surgery = 1;
+	}
+	else{
+		document.getElementById("firstchpt-tag-surgery").style.border=deselectBorder;
+		surgery = 0;
+	}
+}
+function famTag(){
+	if (fam == 0){
+		document.getElementById("firstchpt-tag-family").style.border=selectBorder;
+		fam = 1;
+	}
+	else{
+		document.getElementById("firstchpt-tag-family").style.border=deselectBorder;
+		fam = 0;
+	}
+}
+function refTag(){
+	if (ref == 0){
+		document.getElementById("firstchpt-tag-reflection").style.border=selectBorder;
+		ref = 1;
+	}
+	else{
+		document.getElementById("firstchpt-tag-reflection").style.border=deselectBorder;
+		ref = 0;
+	}
+}
